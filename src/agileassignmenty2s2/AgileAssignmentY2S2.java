2f3dcda8b99fec557d860d75cf088c3a56adc136
Lenovo <Lenovo@ZenK>
2017-11-11 17:15:00 +0800

    Version 1.0

--- src/agileassignmenty2s2/AgileAssignmentY2S2.java
+++ src/agileassignmenty2s2/AgileAssignmentY2S2.java
@@ -13,27 +13,43 @@ import java.util.Scanner;
  */
 public class AgileAssignmentY2S2 {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		// TODO code application logic here
-		Scanner scanner = new Scanner(System.in);
-		DeliveryMan deliveryMan = new DeliveryMan();
-		
-		System.out.print("Enter name               : ");
-		deliveryMan.setName(scanner.nextLine());
-		System.out.print("Enter status             : ");
-		deliveryMan.setStatus(scanner.nextLine());
-		System.out.print("Enter age                : ");
-		deliveryMan.setAge(scanner.nextInt());
-		System.out.print("Enter number of delivery : ");
-		deliveryMan.setNumberOfDelivery(scanner.nextInt());
-		System.out.print("Enter salary             : ");
-		deliveryMan.setSalary(scanner.nextDouble());
-		
-		System.out.println(deliveryMan.toString());
-		
-	}
 	
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		DeliveryMan deliveryMan;
+		HrManager hr = new HrManager();
+		boolean spacing = false;
+
+		do {
+			if (spacing) {
+				System.out.println("\n\n\n\n");
+			}
+
+			deliveryMan = new DeliveryMan();
+			
+			System.out.print("Enter name               : ");
+			deliveryMan.setName(scanner.nextLine());
+			System.out.print("Enter status             : ");
+			deliveryMan.setStatus(scanner.nextLine());
+			System.out.print("Enter age                : ");
+			deliveryMan.setAge(scanner.nextInt());
+			System.out.print("Enter number of delivery : ");
+			deliveryMan.setNumberOfDelivery(scanner.nextInt());
+			System.out.print("Enter salary             : ");
+			deliveryMan.setSalary(scanner.nextDouble());
+
+			hr.add(deliveryMan);
+
+			System.out.print("Do you wish to continue to add staff member? (Y/N)");
+			spacing = true;
+
+		} while (scanner.nextLine().equals("Y") || scanner.nextLine().equals("y"));
+
+		System.out.println("\n\n\n\n");
+		System.out.println("Delivery Man List: ");
+		System.out.println(hr.toString());
+	}
+
 }
