5c7093ef1846a8d61df446d481dc0fbbc59a196f
Lenovo <Lenovo@ZenK>
2017-11-15 21:20:56 +0800

    Version 1.0

--- src/agileassignmenty2s2/AgileAssignmentY2S2.java
+++ src/agileassignmenty2s2/AgileAssignmentY2S2.java
@@ -13,58 +13,57 @@ import java.util.Scanner;
  */
 public class AgileAssignmentY2S2 {
 
-	
 	public static void main(String[] args) {
-		
-		
+
 		Scanner scanner = new Scanner(System.in);
 		DeliveryMan deliveryMan;
 		HrManager hr = new HrManager();
 		boolean spacing = false;
-                    int answer;
-                
-                do {
-                    System.out.println("1. Add Delivery Man");
-                System.out.println("2. View Delivery Man Detailed");
-                System.out.println("3. End");
-                 answer = Integer.parseInt(scanner.nextLine());
-                
-                if (answer == 1) {
+		int answer;
+
 		do {
-			if (spacing) {
+			System.out.println("1. Add Delivery Man");
+			System.out.println("2. View Delivery Man Detailed");
+			System.out.println("3. End");
+			answer = Integer.parseInt(scanner.nextLine());
+
+			if (answer == 1) {
+				do {
+					if (spacing) {
+						System.out.println("\n\n\n\n");
+					}
+
+					deliveryMan = new DeliveryMan();
+
+					System.out.print("Enter name               : ");
+					deliveryMan.setName(scanner.nextLine());
+					System.out.print("Enter status             : ");
+					deliveryMan.setStatus(scanner.nextLine());
+					System.out.print("Enter age                : ");
+					deliveryMan.setAge(scanner.nextInt());
+					deliveryMan.setNumberOfDelivery(0);
+					System.out.print("Enter salary             : ");
+					deliveryMan.setSalary(scanner.nextDouble());
+					System.out.print("Enter address            : ");
+					deliveryMan.setAddress(scanner.nextLine());
+					System.out.print("Enter phone number       : ");
+					deliveryMan.setPhoneNumber(scanner.nextLine());
+
+					hr.add(deliveryMan);
+
+					System.out.print("Do you wish to continue to add staff member? (Y/N)");
+					spacing = true;
+
+				} while (scanner.nextLine().equals("Y") || scanner.nextLine().equals("y"));
+				
+			} else if (answer == 2) {
+				
 				System.out.println("\n\n\n\n");
+				System.out.println("Delivery Man List: ");
+				System.out.println(hr.toString());
 			}
+		} while (answer != 3);
 
-			deliveryMan = new DeliveryMan();
-			
-			System.out.print("Enter name               : ");
-			deliveryMan.setName(scanner.nextLine());
-			System.out.print("Enter status             : ");
-			deliveryMan.setStatus(scanner.nextLine());
-			System.out.print("Enter age                : ");
-			deliveryMan.setAge(scanner.nextInt());
-			System.out.print("Enter number of delivery : ");
-			deliveryMan.setNumberOfDelivery(scanner.nextInt());
-			System.out.print("Enter salary             : ");
-			deliveryMan.setSalary(scanner.nextDouble());
-
-			hr.add(deliveryMan);
-
-			System.out.print("Do you wish to continue to add staff member? (Y/N)");
-			spacing = true;
-
-		} while (scanner.nextLine().equals("Y") || scanner.nextLine().equals("y"));
-                }
-                else if (answer == 2) {
-                    System.out.println("\n\n\n\n");
-		System.out.println("Delivery Man List: ");
-		System.out.println(hr.toString());
-                }
-                } while (answer != 3); 
-                
-                
-		
 	}
-                
 
 }
